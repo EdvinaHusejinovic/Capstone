b2ef96a5fb844e2bcf211da7b405a6ea638919b7
Edvina Husejinovic <edvinahusejinovic@gmail.com>
2023-12-24 14:09:32 -0600

    Updates and OpenWeather API

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,16 @@
 import html from "html-literal";
+import logo from "../assets/img/dandelion.png";
 
 export default state => html`
   <header>
+    <img src=${logo} alt="logo" />
     ${state.header}
+    <nav class="navigator">
+      <ul>
+        <li><a href=${state.home}>Home</a></li>
+        <li><a href=${state.about}>About</a></li>
+        <li><a href=${state.contact}>Contact</a></li>
+      </ul>
+    </nav>
   </header>
 `;
--- store/Administration.js
+++ store/Administration.js
@@ -1,4 +1,4 @@
 export default {
   header: "Administration Page",
-  view: "Administration "
+  view: "Administration"
 };
