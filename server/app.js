8a6a34051da6573dcb09c385b84f1a83ab4ab740
Edvina Husejinovic <edvinahusejinovic@gmail.com>
2023-12-29 20:56:05 -0600

    HTTP node server, middleware and MongoDB

---
+++ server/app.js
@@ -0,0 +1,62 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Load environment variables from .env files
+dotenv.config();
+
+const app = express();
+
+// MongoDB
+mongoose.connect(process.env.MONGODB, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// map the constant db to mongoose.connection
+const db = mongoose.connection;
+
+// tell us that the connection to the database was successful
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+// get the PORT from the environment variables, OR use 4040 as default
+const PORT = process.env.PORT || 4040;
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+// Logging Middleware
+const logging = (request, response, next) => {
+  console.log(
+    `${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`
+  );
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
+
+// Request handlers go here
+app.get("/status", (request, response) => {
+  response.status(200).json({ message: "Service healthy" });
+});
+
+app.listen(PORT, () => console.log("Listening on port 4040"));
